c2c2dbb46da0e3322a9721b5de9c962b8480c1c1
keenwon <semanwmj@gmail.com>
2021-10-20 22:34:33 +0800

    chroe

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,48 +1,65 @@
 import path from "path"
 import {fileURLToPath} from "url"
+import webpack from "webpack"
 import HtmlWebpackPlugin from "html-webpack-plugin"
 
-const dirname = path.dirname(fileURLToPath(import.meta.url))
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 const getAbsPath = (relativePath) => path.resolve(dirname, relativePath)
 
 const publicPath = "/"
 
-export default {
-  entry: {
-    app: [getAbsPath("src/index")],
-  },
-  output: {
-    filename: "[name].js",
-    path: getAbsPath("dist"),
-    publicPath,
-  },
-  target: "web",
-  mode: "development",
-  resolve: {
-    extensions: [".ts", ".tsx", ".cjs", ".mjs", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|mjs|cjs|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
-  },
-  plugins: [new HtmlWebpackPlugin()],
-  devServer: {
-    host: "0.0.0.0",
-    hot: true,
-    client: {
-      overlay: true,
+export default () => {
+  process.env.NODE_ENV = "development"
+
+  return {
+    entry: {
+      app: [getAbsPath("src/index")],
     },
-    historyApiFallback: {
-      disableDotRule: true,
-    },
-    static: {
-      directory: getAbsPath("dist"),
+    output: {
+      filename: "[name].js",
+      path: getAbsPath("dist"),
       publicPath,
     },
-  },
+    target: "web",
+    mode: "development",
+    resolve: {
+      extensions: [".ts", ".tsx", ".cjs", ".mjs", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|mjs|cjs|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+      ],
+    },
+    cache: {
+      type: "filesystem",
+      buildDependencies: {
+        config: [filename],
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
+    devServer: {
+      host: "0.0.0.0",
+      hot: true,
+      client: {
+        overlay: true,
+      },
+      historyApiFallback: {
+        disableDotRule: true,
+      },
+      static: {
+        directory: getAbsPath("dist"),
+        publicPath,
+      },
+    },
+  }
 }
